217f80012d1eaaaab7be71fe47003e39407c69ff
Lakhan Mankani <lakhan.mankani@gmail.com>
2021-07-13 20:37:14 +0100

    Simplify samecalendar calculation
    Signed-off-by: Lakhan Mankani <lakhan.mankani@gmail.com>

--- samecalendar/samecalendar.go
+++ samecalendar/samecalendar.go
@@ -21,69 +21,45 @@ func yearStartDay(year int) time.Weekday {
 	return date.Weekday()
 }
 
-func yearsWithSameLeapness(year int, n int) []int {
-	var years []int
-	yearLeapness := isLeapYear(year)
-
-	for len(years) < n {
-		if isLeapYear(year) == yearLeapness {
-			years = append(years, year)
-		}
-		year += 1
+func nextYearWithSameLeapness(year int) (nextYear int) {
+	isLeap := isLeapYear(year)
+	nextYear = year + 1
+	for isLeapYear(nextYear) != isLeap {
+		nextYear += 1
 	}
-	return years
+	return nextYear
 }
 
-func yearsWithSameStartDay(year int, n int) []int {
-	var years []int
+func nextYearWithSameStartDay(year int) (nextYear int) {
 	startDay := yearStartDay(year)
+	nextYear = year + 5
 
-	for len(years) < n {
-		if yearStartDay(year) == startDay {
-			years = append(years, year)
-		}
-		year += 1
+	for yearStartDay(nextYear) != startDay {
+		nextYear += 1
 	}
-	return years
-}
-
-func intersection(a, b []int) (c []int) {
-	m := make(map[int]bool)
-
-	for _, item := range a {
-		m[item] = true
-	}
-
-	for _, item := range b {
-		if _, ok := m[item]; ok {
-			c = append(c, item)
-		}
-	}
-	return
+	return nextYear
 }
 
 func SameCalendar(year int, n int) ([]int, error) {
-	if n == 1 {
-		return []int{year}, nil
-	}
 	if n < 0 {
 		err := errors.New("n must be greater than 0")
 		return nil, err
 	}
-	var years []int
+	var years = []int{year}
 
 	for len(years) < n {
-		var lastYear int
-		if len(years) == 0 {
-			lastYear = year
-		} else {
-			lastYear = years[len(years)-1]
-		}
+		lastYear := years[len(years)-1]
 
-		sameLeapness := yearsWithSameLeapness(lastYear, 10*n)
-		sameStartDate := yearsWithSameStartDay(lastYear, 10*n)
-		yearIntersection := intersection(sameLeapness, sameStartDate)
-		years = append(years, yearIntersection...)
+		nextSameLeap := nextYearWithSameLeapness(lastYear)
+		nextSameDay := nextYearWithSameStartDay(lastYear)
+		for nextSameLeap != nextSameDay {
+			if nextSameLeap > nextSameDay {
+				nextSameDay = nextYearWithSameStartDay(nextSameDay)
+			} else if nextSameDay > nextSameLeap {
+				nextSameLeap = nextYearWithSameLeapness(nextSameLeap)
+			}
+		}
+		years = append(years, nextSameLeap)
 	}
-	return years[:n], nil
+	return years, nil
 }
